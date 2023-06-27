89dd2d840038778f1269d504c800053fe4e8f8bf
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-27 16:54:14 +0300

    confirmed all references

--- db/migrate/20230627131139_create_recipes.rb
+++ db/migrate/20230627131139_create_recipes.rb
@@ -1,12 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
-    create_table :recipes do |t|
+    create_table :recipes, id: :primary_key do |t|
       t.string :Name
       t.timestamps :PreparationTime
       t.timestamps :CookingTime
       t.string :Description
       t.boolean :Public
-      t.references :user, foreign_key: true
+      t.references :users, foreign_key: true
       t.timestamps
     end
   end
