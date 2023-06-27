3ba9277c3a5c04b249a27f895f2c54b2a28cdae8
MICHAELKITH <michaelkithinji27@gmail.com>
2023-06-27 17:53:17 +0300

    Add github workflows

--- db/migrate/20230627125631_create_foods.rb
+++ db/migrate/20230627125631_create_foods.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods, id: :primary_key do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,29 +12,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_130750) do
+ActiveRecord::Schema[7.0].define(version: 20_230_627_130_750) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "food_recipes", force: :cascade do |t|
-    t.integer "Quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'food_recipes', force: :cascade do |t|
+    t.integer 'Quantity'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "foods", force: :cascade do |t|
-    t.string "Name"
-    t.string "MeasurementUnit"
-    t.decimal "Price"
-    t.integer "Quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'foods', force: :cascade do |t|
+    t.string 'Name'
+    t.string 'MeasurementUnit'
+    t.decimal 'Price'
+    t.integer 'Quantity'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "Name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'Name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
