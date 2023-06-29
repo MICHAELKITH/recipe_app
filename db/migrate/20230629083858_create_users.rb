66310714523e1860ba47785ffa71c5ebdf5c8cab
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 11:47:45 +0300

    Updated the migrations

--- db/migrate/20230629083858_create_users.rb
+++ db/migrate/20230629083858_create_users.rb
@@ -2,8 +2,9 @@
 
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users, id: :primary_key do |t|
-      t.string :Name
+    create_table :users do |t|
+      t.string :name
+
       t.timestamps
     end
   end
