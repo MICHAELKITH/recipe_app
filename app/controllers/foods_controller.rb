f2c3ce5e930d45d98db7fbafea5fcfcc470c4dc2
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 13:24:09 +0300

    Added devise controllers

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,36 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      flash[:success] = 'Food succesfully added'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Error: Food could not be added'
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    flash[:notice] = 'You deleted food successfully'
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_091602) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_100237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,8 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_091602) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
-    t.string "encrypted_password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
