a1b98c7c841b0feb626273f37e74d3387dc01880
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 11:47:45 +0300

    Updated the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,28 +12,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_627_130_750) do
+ActiveRecord::Schema[7.0].define(version: 20_230_629_084_119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
-  create_table 'food_recipes', force: :cascade do |t|
-    t.integer 'Quantity'
+  create_table 'foods', force: :cascade do |t|
+    t.string 'name'
+    t.string 'measurement_unit'
+    t.decimal 'price'
+    t.integer 'quantity'
+    t.bigint 'user_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_foods_on_user_id'
   end
 
-  create_table 'foods', force: :cascade do |t|
-    t.string 'Name'
-    t.string 'MeasurementUnit'
-    t.decimal 'Price'
-    t.integer 'Quantity'
+  create_table 'recipe_foods', force: :cascade do |t|
+    t.integer 'quantity'
+    t.bigint 'recipe_id', null: false
+    t.bigint 'food_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['food_id'], name: 'index_recipe_foods_on_food_id'
+    t.index ['recipe_id'], name: 'index_recipe_foods_on_recipe_id'
+  end
+
+  create_table 'recipes', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'preparation_time'
+    t.integer 'cooking_time'
+    t.text 'description'
+    t.boolean 'public'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_recipes_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'Name'
+    t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+
+  add_foreign_key 'foods', 'users'
+  add_foreign_key 'recipe_foods', 'foods'
+  add_foreign_key 'recipe_foods', 'recipes'
+  add_foreign_key 'recipes', 'users'
 end
