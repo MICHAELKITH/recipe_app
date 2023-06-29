64a2af9219b431d01830c1789f445d0372bd4fc5
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 13:24:09 +0300

    Added devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  root 'public_recipes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :recipes, only: [:index, :show, :create, :destroy] do
+    resources :recipe_foods, except: :update
+  end
+
+  resources :foods, only: [:index, :show, :create, :new, :destroy] 
+  resources :shopping_lists, only: [:index]
 end
