dac642a1de6c8c6a26368bb2b7d3ca6b52b2ad2f
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 10:28:00 +0300

    Rolled back and completed migration

--- db/migrate/20230629064801_add_user_id_to_food_recipes.rb
+++ db/migrate/20230629064801_add_user_id_to_food_recipes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserIdToFoodRecipes < ActiveRecord::Migration[7.0]
   def change
     add_reference :food_recipes, :user, null: false, foreign_key: true
--- db/migrate/20230629065721_add_foreign_keys_to_food_recipes.rb
+++ db/migrate/20230629065721_add_foreign_keys_to_food_recipes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddForeignKeysToFoodRecipes < ActiveRecord::Migration[7.0]
   def change
     add_reference :food_recipes, :recipe, null: false, foreign_key: true
