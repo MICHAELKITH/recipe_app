810d759ebedbd38ec4ecbd6ee4c312ababde4b5f
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-29 16:31:16 +0300

    Added Recipe list and enable access by only public allowed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,40 @@
 class RecipesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!, except: [:show]
 
-  def show; end
+  def index
+    @recipes = current_user.recipes
+  end
 
-  def create; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-  def destroy; end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      flash[:success] = 'Recipe succesfully added'
+      redirect_to recipes_path
+    else
+      flash[:error] = 'Error: Recipe could not be added'
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:notice] = 'You deleted Recipe successfully'
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
