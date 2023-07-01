a75d664dfa161f72e28d706f50d1036f4cd6bc2a
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-01 08:01:40 +0300

    Add unit tests

--- Gemfile
+++ Gemfile
@@ -69,10 +69,11 @@ group :development do
   # gem "spring"
 end
 
-group :test do
+group :development, :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
 gem 'cancancan'
---
+++ spec/controllers/food_recipes_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  let(:recipe) { Recipe.new(name: 'Pizza') }
+  let(:food) { Food.new(name: 'Cheese', measurement_unit: 'grams', price: 10, quantity: 10) }
+
+  subject(:recipe_food) { described_class.new(recipe: recipe, food: food, quantity: 2) }
+
+  it 'belongs to a recipe' do
+    expect(recipe_food.recipe).to eq(recipe)
+  end
+
+  it 'belongs to a food' do
+    expect(recipe_food.food).to eq(food)
+  end
+
+  it 'has a quantity' do
+    expect(recipe_food.quantity).to eq(2)
+  end
+end
